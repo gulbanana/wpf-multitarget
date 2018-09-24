be0f0a298b76264a21fc53d8c6598cbc41c514d9
Thomas Castiglione <castiglione@mac.com>
2018-09-24 16:47:22 +0800

    add a .NET Framework app and multitarget the control library

---
+++ netapp/App.xaml.cs
@@ -0,0 +1,6 @@
+﻿using System.Windows;
+
+namespace netapp
+{
+    public partial class App : Application { }
+}
---
+++ netapp/GreenBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace netapp
+{
+    public class GreenBox : Control
+    {
+        static GreenBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GreenBox), new FrameworkPropertyMetadata(typeof(GreenBox)));
+        }
+    }
+}
